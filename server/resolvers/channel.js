9faf611e82972719ed873b2a533ed23d992a62ad
Kunle Elegbede <kunle_elegbede@hotmail.com>
2018-10-08 16:59:47 +0100

    initial commit - completed basic server infrastructure

---
+++ server/resolvers/channel.js
@@ -0,0 +1,17 @@
+export default {
+    Query: {
+
+    },
+    Mutation: {
+        createChannel: async(parent, args, { models }) => {
+            try{
+                await models.Channel.create(args);
+                return true;
+            }
+            catch(err){
+                console.log(err);
+                return false;
+            }
+        }
+    }
+};
